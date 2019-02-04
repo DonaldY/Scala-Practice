6cdc60a9ce1ea11224d3cb9aa009bfbd425dc9fd
Donald <448641125@qq.com>
2019-02-04 23:48:42 +0800

    update trait

---
+++ src/main/scala/trait/Decorator.scala
@@ -0,0 +1,18 @@
+package `trait`
+
+abstract class Check {
+  def check: String = "Checked Application Details..."
+}
+
+
+trait CreditCheck extends Check {
+  override def check: String = s"Checked Credit... ${super.check}"
+}
+
+trait EmploymentCheck extends Check {
+  override def check: String = s"Checked Employment... ${super.check}"
+}
+
+trait CriminalRecordCheck extends Check {
+  override def check: String = s"Check Criminal Records... ${super.check}"
+}
