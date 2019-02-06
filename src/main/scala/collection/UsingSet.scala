392baa8324b82336a10d1a6a101b29cefb371c54
Donald <448641125@qq.com>
2019-02-06 23:43:12 +0800

    init collection

---
+++ src/main/scala/collection/UsingSet.scala
@@ -0,0 +1,11 @@
+package collection
+
+object UsingSet extends App {
+
+  val colors1 = Set("Blue", "Green", "Red")
+  println(s"colors1: $colors1")
+
+  val colors2 = colors1 + "Black"
+  println(s"colors2: $colors2")
+  println(s"colors1: $colors1")
+}
