b586c5243288eb2c22e1429e6c750ec72299f418
Donald <448641125@qq.com>
2019-02-07 10:12:26 +0800

    update set

--- src/main/scala/collection/UsingSet.scala
+++ src/main/scala/collection/UsingSet.scala
@@ -8,4 +8,22 @@ object UsingSet extends App {
   val colors2 = colors1 + "Black"
   println(s"colors2: $colors2")
   println(s"colors1: $colors1")
+
+  val feeds1 = Set("blog.toolshed.com", "pragdave.me", "blog.agiledeveloper.com")
+  val feeds2 = Set("blog.toolshed.com", "martifowler.com/bliki")
+
+  val blogFeeds = feeds1 filter (_ contains "blog")
+  println(s"blog feeds: ${blogFeeds.mkString(",")}")
+
+  val mergedFeeds = feeds1 ++ feeds2
+  println(s"# of merged feeds: ${mergedFeeds.size}")
+
+  val commonFeeds = feeds2 & feeds1
+  println(s"common feeds: ${commonFeeds.mkString(", ")}")
+
+  val urls = feeds1 map ("http://" + _)
+  println(s"One url: ${urls.head}")
+
+  println("Refresh Feeds:")
+  feeds1 foreach { feed => println(s" Refreshing $feed...")}
 }
