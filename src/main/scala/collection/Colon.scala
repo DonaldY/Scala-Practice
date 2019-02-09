e8840eea877cdb86df0057b68df9d3637afe7371
Donald <448641125@qq.com>
2019-02-10 00:00:28 +0800

    update function

--- src/main/scala/collection/Colon.scala
+++ src/main/scala/collection/Colon.scala
@@ -3,3 +3,15 @@ package collection
 class Cow {
   def ^(moon: Moon): Unit = println("Cow jumped over the moon")
 }
+
+class Moon {
+  def ^:(cow: Cow): Unit = println("This cow jumped over the moon too")
+}
+
+object Colon extends App {
+  val cow = new Cow
+  val moon = new Moon
+
+  cow ^ moon
+  cow ^: moon
+}
