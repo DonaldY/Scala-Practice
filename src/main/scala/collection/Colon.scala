e8118b3549fd34c31465404f5b663eeb1191674f
Donald <448641125@qq.com>
2019-02-10 00:03:14 +0800

    add unary

--- src/main/scala/collection/Colon.scala
+++ src/main/scala/collection/Colon.scala
@@ -8,10 +8,24 @@ class Moon {
   def ^:(cow: Cow): Unit = println("This cow jumped over the moon too")
 }
 
+class Sample {
+
+  def unary_+(): Unit = println("Called unary +")
+  def unary_-(): Unit = println("Called unary -")
+  def unary_!(): Unit = println("Called unary !")
+  def unary_~(): Unit = println("called unary ~")
+}
+
 object Colon extends App {
   val cow = new Cow
   val moon = new Moon
 
   cow ^ moon
   cow ^: moon
+
+  val sample = new Sample
+  +sample
+  -sample
+  !sample
+  ~sample
 }
