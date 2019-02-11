a3cf5c8e843af1c8c1f6998cf33c147eb757e303
Donald <448641125@qq.com>
2019-02-11 19:10:30 +0800

    add friends

---
+++ src/main/scala/collection/Friends.scala
@@ -0,0 +1,20 @@
+package collection
+
+class Person (val firstName: String, val lastName: String)
+object Person {
+  def apply(firstName: String,lastName: String): Person =
+    new Person(firstName, lastName)
+}
+
+object Friends extends App {
+
+  val friends = List(Person("Brian", "Sletten"), Person("Neal", "Ford"),
+    Person("Scott", "Davis"), Person("Stuart", "Halloway"))
+
+  val lastNames =
+    for (friend <- friends; lastName = friend.lastName) yield lastName
+
+  println(lastNames.mkString(", "))
+}
+
+
