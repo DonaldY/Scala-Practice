417607c8abb4304c527b902c616393de8a79bba7
Donald <448641125@qq.com>
2019-02-12 19:30:07 +0800

    update recursive

--- src/main/scala/recursive/Factorial.scala
+++ src/main/scala/recursive/Factorial.scala
@@ -9,4 +9,16 @@ object Factorial extends App {
     else
       number * factorial(number - 1)
   }
+
+  // 确保尾调用优化
+  // 如果不是尾调用会编译异常，编辑器也会报错
+  @scala.annotation.tailrec
+  def factorial(fact: BigInt, number: Int): BigInt = {
+    if (number == 0)
+      fact
+    else
+      factorial(fact * number, number - 1)
+  }
+
+  println(factorial(1, 10000))
 }
