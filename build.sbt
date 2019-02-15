bf7f2311f7a9116833f6ac79af567472c15fefff
Donald <448641125@qq.com>
2019-02-15 19:28:53 +0800

    update akka

--- build.sbt
+++ build.sbt
@@ -3,3 +3,6 @@ name := "Scala-Practice"
 version := "0.1"
 
 scalaVersion := "2.12.8"
+
+// https://mvnrepository.com/artifact/com.typesafe.akka/akka-actor
+libraryDependencies += "com.typesafe.akka" %% "akka-actor" % "2.5.21"
--- src/main/scala/actors/HollywoodActor.scala
+++ src/main/scala/actors/HollywoodActor.scala
@@ -5,6 +5,6 @@ import akka.actor.{AbstractActor, Actor}
 class HollywoodActor extends Actor {
 
   override def receive: Receive = {
-    case message => println(s"playing the role of $message")
+    case message => println(s"$message - ${Thread.currentThread()}")
   }
 }
