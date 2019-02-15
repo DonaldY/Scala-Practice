d0c7130373017ea75220a9ca30000e4b300bf9a8
Donald <448641125@qq.com>
2019-02-15 19:32:35 +0800

    update

--- src/main/scala/actors/CreateActors.scala
+++ src/main/scala/actors/CreateActors.scala
@@ -10,10 +10,15 @@ object CreateActors extends App {
   val system = ActorSystem("sample")
 
   val depp = system.actorOf(Props[HollywoodActor])
+  val hanks = system.actorOf(Props[HollywoodActor])
 
   depp ! "Wonka"
+  hanks ! "Gump"
 
-  depp.tell("Wooo", depp)
+  depp ! "Sparrow"
+  hanks ! "Phillips"
+
+  println(s"Calling from ${Thread.currentThread()}")
 
   val terminateFuture = system.terminate()
   Await.ready(terminateFuture, Duration.Inf)
