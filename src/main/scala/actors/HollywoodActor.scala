329c3a9716f8ae859d395da30ea9f7a952bb4341
Donald <448641125@qq.com>
2019-02-17 14:06:03 +0800

    update HollywoodActor

--- src/main/scala/actors/HollywoodActor.scala
+++ src/main/scala/actors/HollywoodActor.scala
@@ -1,10 +1,24 @@
 package actors
 
 import akka.actor.{AbstractActor, Actor}
+import scala.collection._
+
+case class Play(role: String)
+case class ReportCount(role: String)
 
 class HollywoodActor extends Actor {
 
   override def receive: Receive = {
-    case message => println(s"$message - ${Thread.currentThread()}")
+    case Play(role) =>
+      val currentCount = messagesCount.getOrElse(role, 0)
+      messagesCount.update(role, currentCount + 1)
+      println(s"Playing $role")
+
+    case ReportCount(role) =>
+      sender ! messagesCount.getOrElse(role, 0)
   }
+
+  val messagesCount: mutable.Map[String, Int] = mutable.Map()
+
+
 }
