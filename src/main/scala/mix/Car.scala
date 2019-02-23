59ffaf94bdf27d0d7209d9a69536729d3b08917d
Donald <448641125@qq.com>
2019-02-23 17:41:50 +0800

    mix Java and Scala

---
+++ src/main/scala/mix/Car.scala
@@ -0,0 +1,10 @@
+package mix
+
+class Car(val year: Int) {
+
+  private[this] var miles: Int = 0
+
+  def drive(distance: Int): Unit = { miles += distance }
+
+  override def toString: String = s"year: $year miles: $miles"
+}
---
+++ src/main/scala/mix/UseCar.java
@@ -0,0 +1,13 @@
+package mix;
+
+public class UseCar {
+
+    public static void main(String[] args) {
+        Car car = new Car(2009);
+
+        System.out.println(car);
+        car.drive(10);
+
+        System.out.println(car);
+    }
+}
