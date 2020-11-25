55344121635c60c5c9a78f9c04cf432c30089090
Donald <448641125@qq.com>
2020-11-25 22:45:35 +0800

    update actor homework

--- src/main/scala/demo/homework/MasterSlaveDemo.scala
+++ src/main/scala/demo/homework/MasterSlaveDemo.scala
@@ -3,84 +3,98 @@ package demo.homework
 import java.util.Date
 import java.util.concurrent.{Executors, TimeUnit}
 
-import akka.actor.{Actor, ActorSystem, Props}
+import akka.actor.{Actor, ActorRef, ActorSystem, Props}
 
 /**
   * @author donald
   * @date 2020/11/25
   */
 
-case class HeartBeatMsg(msg: String)
+case class HeartBeatMsg(user: Master, msg: String)
+case class StopMsg()
 
 object MasterSlaveDemo {
 
-  private val MasterFactory = ActorSystem("MasterFactory")
-
-  private val masterRef = MasterFactory.actorOf(Props[Master], "master")
-
-
   def main(args: Array[String]): Unit = {
 
-    //new Master("master")
+    val MasterFactory = ActorSystem("MasterFactory")
+    val masterRef = MasterFactory.actorOf(Props[MasterActor], "master")
+
+    val master = new Master()
+    master.init()
 
     val heartbeatTask = new Runnable {
 
       override def run(): Unit = {
 
-        masterRef ! HeartBeatMsg("workerA")
+        masterRef ! HeartBeatMsg(master, "workerA")
       }
     }
 
     val workerService = Executors.newSingleThreadScheduledExecutor()
-    workerService.scheduleAtFixedRate(heartbeatTask, 3, 1, TimeUnit.SECONDS)
+    workerService.scheduleAtFixedRate(heartbeatTask, 1, 3, TimeUnit.SECONDS)
 
+    Thread.sleep(10000)
 
-    Thread.sleep(20000)
+    workerService.shutdownNow()
+
+    Thread.sleep(10000)
+
+    master.stop(masterRef)
+  }
+}
+
+class MasterActor extends Actor {
+
+  override def receive: Receive = {
+
+    case HeartBeatMsg(master, workerName) => master.updateWorker(workerName)
+    case StopMsg() =>
+      context.stop(self)
+      context.system.terminate()
+  }
+}
+
+class Master {
+
+  private val service = Executors.newSingleThreadScheduledExecutor()
+
+  var workerMap: Map[String, Date] = Map()
+
+  def stop(actorRef: ActorRef): Unit = {
+
+    actorRef ! StopMsg()
+
+    service.shutdownNow()
   }
 
-  class Master extends Actor{
+  def init(): Unit = {
 
-    private val service = Executors.newSingleThreadScheduledExecutor()
+    val cleanTask = new Runnable {
+      override def run(): Unit = {
 
-    var workerMap: Map[String, Date] = Map()
+        val currDate = new Date()
 
-    def this(name: String) {
+        for ((k, v) <- workerMap) {
 
-      this()
+          val second = (currDate.getTime - v.getTime) / 1000
 
-      val cleanTask = new Runnable {
-        override def run(): Unit = {
+          if (second > 5) {
 
-          val currDate = new Date()
-
-          for ((k, v) <- workerMap) {
-
-            val second = (currDate.getTime - v.getTime) / 1000
-
-            if (second > 10) {
-
-              println(s"master 移除 ： $k")
-              workerMap -= k
-            }
+            println(s"master 移除 ： $k")
+            workerMap -= k
           }
         }
       }
-
-      this.service.scheduleAtFixedRate(cleanTask, 5, 1, TimeUnit.SECONDS)
     }
 
-    def updateWorker(workerName: String): Unit = {
-
-      println(s"master 接收 heartbeat : $workerName")
-
-      workerMap += (workerName -> new Date())
-    }
-
-    override def receive: Receive = {
-
-      case HeartBeatMsg(workerName) => updateWorker(workerName)
-    }
+    this.service.scheduleAtFixedRate(cleanTask, 1, 3, TimeUnit.SECONDS)
   }
 
-}
+  def updateWorker(workerName: String): Unit = {
 
+    println(s"master 接收 $workerName 的 heartbeat ")
+
+    workerMap += (workerName -> new Date())
+  }
+}
